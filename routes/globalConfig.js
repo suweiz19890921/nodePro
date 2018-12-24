f28967be7db478309c3904c190f4c0dc38ca0661
苏威 <suwei@solot.com>
2018-12-24 15:52:38 +0800

    [public] sql test;

--- routes/globalConfig.js
+++ routes/globalConfig.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var JSON = require('JSON');
+
+var config = {
+  host     : 'localhost',
+  user     : 'root',
+  password : '123456',
+  database : 'base',
+  stringifyObjects : 'TRUE'
+}
+
 router.get('/', function (req, res) {
 	res.send('全局配置');
 });
@@ -13,8 +24,23 @@ router.get('/newVersion', function (req, res) {
 
 router.get('/review', function (req, res) {
 	console.log('获取审核状态 版本号为' + req.query.version);
-	var data = {'code':450, 'data':{'isInReview' : 0}};
-	res.send(data);
+	var connection = mysql.createConnection(config);
+	connection.connect();
+    var sql = 'SELECT reviewConfig()'
+    connection.query(sql, function (error, result) {
+    if (error) throw error;
+        console.log('--------------------------SELECT----------------------------');
+        
+       result = JSON.stringify(result);
+       result = JSON.parse(result);//把results字符串转为json对象
+       for(var i=0;i<result.length;i++)
+        {
+            console.log(result[i]['reviewConfig()']);
+        }
+        res.send(result[0]['reviewConfig()']);
+        connection.end();
+        console.log('------------------------------------------------------------\n\n'); 
+});
 	// res.status(500).send({"code":401000});
 });
 

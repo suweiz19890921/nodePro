949cebac42d2e4bded3a407985025be96542af8c
Leo_FC <814325429@qq.com>
2019-03-07 21:56:07 +0800

    [public] msg api;

--- routes/noticeMsg.js
+++ routes/noticeMsg.js
@@ -3,8 +3,15 @@ var router = express.Router();
 
 
 router.get('/count', function (req, res) {
-	console.log(req.originalUrl);
 	var data = {'code':0, 'data':{'count' : 0}};
+	console.log(data);
+	res.send(data);
+	// res.status(500).send({"code":401000});
+});
+
+router.get('/sysMsgList', function (req, res) {
+	console.log(req.originalUrl);
+	var data = {'code':0, 'data':[]};
 	console.log(data);
 	res.send(data);
 	// res.status(500).send({"code":401000});

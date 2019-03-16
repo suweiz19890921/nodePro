32fa11649cf13a7c0bf8d7ac8fcf2041f35d2f4a
Leo_FC <814325429@qq.com>
2019-03-16 22:46:52 +0800

    [public] useragent change;

--- routes/weather.js
+++ routes/weather.js
@@ -6,12 +6,11 @@ var querystring = require('querystring');
 var client = require(ROOT_DIR + '/lib/clientTidePg.js');
 
 var headers = {
-	'User-Agent' : 'GlobalTide/1.0.0 (iOS 12.1; iPhone8,1; zh_Hans; 750*1334; Scale/2.00;wt3mecmbr)',
+	'User-Agent' : 'GlobalTide/3.0.0 (iOS 12.1; iPhone8,1; zh_Hans; 750*1334; Scale/2.00;wt3mecmbr)',
 	'Authorization' : 'Basic OihudWxsKTpHbG9iYWxUaWRl',
 	'Content-Type':'application/x-www-form-urlencoded',
 }
 var host = 'app.catches.com';
-var path ='/angler/v2/stories?method=storiesList&limit=20&receiveType=1';
 var	method = 'get';
 
 //批量天气
@@ -109,6 +108,7 @@ var callback = function  (response) {
 		}
 	})
 	response.on('error', function (error) {
+		console.error(error);
 		res.send(error);
 	})
 }
